ad381a192136b5f86555bfdd99cd3e61fcf569fa
guozy20 <guoziyang20@163.com>
2020-08-12 22:46:29 +0800

    多版本支持

--- 03.spring-cloud-alibaba/01.apache-dubbo/spring-boot-dubbo-example/spring-boot-dubbo-provider/src/main/java/com/guozy/springboot/dubbo/springbootdubboprovider/service/SayHelloServiceImpl2.java
+++ 03.spring-cloud-alibaba/01.apache-dubbo/spring-boot-dubbo-example/spring-boot-dubbo-provider/src/main/java/com/guozy/springboot/dubbo/springbootdubboprovider/service/SayHelloServiceImpl2.java
@@ -4,10 +4,10 @@ package com.guozy.springboot.dubbo.springbootdubboprovider.service;
 import com.guozy.springboot.dubbo.ISayHelloService;
 import org.apache.dubbo.config.annotation.DubboService;
 
-@DubboService(registry = {"shanghai", "beijing"},version = "1.0")
-public class SayHelloServiceImpl implements ISayHelloService {
+@DubboService(registry = {"shanghai", "beijing"},version = "2.0")
+public class SayHelloServiceImpl2 implements ISayHelloService {
     @Override
     public String sayHello(String msg) {
-        return "hello world！" + msg + "version1.0";
+        return "hello world！" + msg + "version2.0";
     }
 }
